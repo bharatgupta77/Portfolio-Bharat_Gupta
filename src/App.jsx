2521b1248db03dc82dab07636a90c72d239bf9cb
Bharat Gupta <guptabharat297@gmail.com>
2025-03-06 23:24:35 -0600

    Added NavBar component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import EducationTimeline from "./sections/Education/EducationTimeline";
 import Footer from "./sections/Footer/Footer";
 import Hero from "./sections/Hero/Hero";
+import NavBar from "./sections/NavBar/Navbar";
 import Projects from "./sections/Projects/Projects";
 import Skills from "./sections/Skills/Skills";
 
 function App() {
   return (
     <>
+      <NavBar />
       <Hero />
       <EducationTimeline />
       <Projects />
